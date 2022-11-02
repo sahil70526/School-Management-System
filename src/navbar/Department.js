56a54b9d0ce702065c37d1c95ccb739d0aded9da
unknown <sahilsingh70526@gmail.com>
2022-11-02 19:08:02 +0530

    third

--- src/navbar/Department.js
+++ src/navbar/Department.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Department() {
+  const [num, setNum] = useState(["Department"]);
+  const content = ["Student", "faculty", "Administration","Bsiness"];
+
+  function item() {
+    const items = content.map((e) => 
+      <ul><li>{e}</li></ul>
+    );
+
+    setNum(items)
+    console.log(items);
+  }
   return (
     <div >
-        <h1>Department</h1>
+
+      <h1  onClick={item}>{num}</h1> 
     </div>
   )
 }

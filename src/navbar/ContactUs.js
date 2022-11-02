371a2469dddf31f15806745ac5ebc212a16c0cda
unknown <sahilsingh70526@gmail.com>
2022-11-02 19:27:34 +0530

    update

--- src/navbar/ContactUs.js
+++ src/navbar/ContactUs.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Contact() {
+  const [contact,setContact]=useState(["Contact Us"]);
+  const person=["abc@.com","hdg#.com","fgfj%.com"];
+  function update(){
+    const item=person.map((e)=>
+    <ul>
+      <li>{e}</li>
+    </ul>);
+    setContact(item);
+  }
   return (
     <div>
-      <h1>Contact Us</h1>
+      <h1 onClick={update}>{contact}</h1>
     </div>
   )
 }
